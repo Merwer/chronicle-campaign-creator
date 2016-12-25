9bf07b0fbe52449642bcb3a994444c17c0f984f3
Bill Belanger <Bill Belanger>
2016-12-25 10:26:03 -0500

    Added support for saving a zip file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Campaign } from './model/campaign';
 import { Component } from '@angular/core';
 
+declare let JSZip: any;
+declare let saveAs: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,4 +13,26 @@ export class AppComponent {
   title = 'Campaign Creator';
 
   campaign = new Campaign();
+
+  createCampaignFile = function () {
+    console.log("Test");
+    /*
+      var forms = document.querySelectorAll('form');
+      var campaignForm = forms[0];
+      var levelForm = forms[1];
+      var serializeOptions = { 'includeEmpty': false, 'encodes': { 'checkbox': true, 'number': true } };
+      var campaign = $(campaignForm).serializeObject(serializeOptions); // This is the only usage of jQuery.
+      var level = $(levelForm).serializeObject(serializeOptions); // This is the only usage of jQuery.
+      console.log("Campaign", campaign);
+      console.log("Level", level);
+      return false;
+      */
+      var zip = new JSZip();
+      var file = zip.file("campaign.json", "Hello World");
+      zip.generateAsync({type:"blob"})
+          .then(function(content) {
+              // see FileSaver.js
+              saveAs(content, "example.zip");
+          });
+  }
 }
